5e8afd26cb863d3074e19930c41babff835d66f2
Flixirr <filip.0krezel@gmail.com>
2022-08-10 19:00:44 +0200

    removed unnecessary stuff from user documnet

--- Backend/REST API/app/models/user.py
+++ Backend/REST API/app/models/user.py
@@ -7,12 +7,5 @@ class User(Document):
     user_id = StringField(primary_key = True)
     username = StringField()
     password = StringField()
-    trip_history = ListField(ReferenceField(Trip))
-    favourite_trips = ListField(ReferenceField(Trip))
 
-    meta = {'collection': 'mpk-users'}
-
-    def start_session(self):
-        session['logged_in'] = True
-        session['user'] = self
-        return 200
+    meta = {'collection': 'dm-users'}
