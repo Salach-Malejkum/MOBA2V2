204796e2fbd55d056bb826408b330c7cb87fe3c6
Topaz-Frog <erwinos753@gmail.com>
2022-10-02 23:13:31 +0200

    DAR-38,39

---
+++ Assets/Scripts/Game/Tower/HomingMissileController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingMissileController : MonoBehaviour
+{
+    public GameObject target;
+    private float speed = 2f;
+
+    private void Start()
+    {
+        gameObject.transform.TransformPoint(Vector3.zero);
+    }
+
+    private void FixedUpdate()
+    {
+        gameObject.transform.position += (target.transform.position - gameObject.transform.position).normalized * speed * Time.deltaTime;
+        gameObject.transform.LookAt(target.transform);
+        //rb.velocity = transform.up * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        UnityEngine.Debug.Log("Babang");
+        UnityEngine.Debug.Log(other.tag);
+        if (other.gameObject == target)
+        {
+            UnityEngine.Debug.Log("Bang");
+            Destroy(gameObject);
+        }
+    }
+}
