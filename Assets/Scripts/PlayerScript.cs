e37ffaed2668b88824e239d568f6c851769bc81f
Mateusz Salach <mateuszsalach00@gmail.com>
2022-10-11 21:47:58 +0200

    Refactor encapsulation

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,13 +7,13 @@ public class PlayerScript : MonoBehaviour
     private IAttack attackScript;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         attackScript = new AttackPlayerScript(500.0f, 0.5f);
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         if (Input.GetMouseButton(1))
         {
