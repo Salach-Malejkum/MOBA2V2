1d145f1722104f8c85fad02d6269543123b791b4
Filip Krężel <45013654+Flixirr@users.noreply.github.com>
2022-10-18 09:36:22 +0200

    Dar 16 (#12)
    * DAR-122,123
    
    * Update .gitignore
    
    * DAR-107 p1
    
    Co-authored-by: Topaz-Frog <erwinos753@gmail.com>

--- Assets/Scripts/Game/Spawners/ResourceSpawner.cs
+++ Assets/Scripts/Game/Spawners/ResourceSpawner.cs
@@ -22,8 +22,9 @@ public class ResourceSpawner : MonoBehaviour
     private void Spawn()
     {
         GameObject go = Instantiate(this.resourcePrefab, this.transform.position, this.transform.rotation);
-        DestroyResource dr = go.GetComponent<DestroyResource>();
-        dr.spawnerResource = this.gameObject.GetComponent<ResourceSpawner>();
+        MobController mc = go.GetComponent<MobController>();
+        mc.spawnerResource = this.gameObject.GetComponent<ResourceSpawner>();
+        mc.spawner = this.transform;
 
         this.isSpawning = false;
     }
