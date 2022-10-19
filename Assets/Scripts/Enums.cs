2bff0a0d93c075474447083a49c293a8649ce225
Mateusz Salach <mateuszsalach00@gmail.com>
2022-10-19 14:39:34 +0200

    Add spawning point and minion prefabs

--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -1,8 +1,25 @@
+using System.Runtime.CompilerServices;
+using UnityEditor;
+using UnityEngine;
+
 namespace Enums
 {
-    public enum MinionSpawnTime
+    public enum MinionSpawnTime  // Can be changed for the debug option
     {
-        FirstSpawnTimePeriod = 90,
-        NormalSpawnTimePeriod = 30
+        FirstSpawnTimePeriod = 9,
+        NormalSpawnTimePeriod = 3
+    }
+
+    public static class MinionPrefabs
+    {
+        public readonly static GameObject meleeMinion = Resources.Load<GameObject>("Prefabs/MeleeMinion");
+        public readonly static GameObject rangedMinion = Resources.Load<GameObject>("Prefabs/RangedMinion");
+        public readonly static GameObject cannonMinion = Resources.Load<GameObject>("Prefabs/CannonMinion");
+    }
+
+    public static class TeamSpawnerPosition
+    {
+        public readonly static Vector3 redTeamSpawn = new Vector3(-5, 0, 0);
+        public readonly static Vector3 blueTeamSpawn = new Vector3(5, 0, 0);
     }
 }
