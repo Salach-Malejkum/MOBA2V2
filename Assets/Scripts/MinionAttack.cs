12d8399ce978980c2977913a684d774b38aa5543
Salach-Malejkum <mateuszsalach00@gmail.com>
2022-10-24 14:19:34 +0200

    Create utiltiy class, change OnTrigger methods

--- Assets/Scripts/MinionAttack.cs
+++ Assets/Scripts/MinionAttack.cs
@@ -6,9 +6,12 @@ public class MinionAttack
 {
     private float aaCooldown;
     private float timeAfterAA = 0;
+    private float timer = 0;
 
-    public int Attack()
+    public int Attack(GameObject enemy)
     {
+        if (enemy.Equals(null))
+            return -2;
         return 0;
     }
 }
--- Assets/Scripts/MinionScript.cs
+++ Assets/Scripts/MinionScript.cs
@@ -6,20 +6,64 @@ using UnityEngine.AI;
 public class MinionScript : MonoBehaviour
 {
     private IMinionMovement minionMovement;
+    private MinionAttack minionAttack;
+    private bool followAttack = false;
+    private HashSet<GameObject> objectsInRangeHashSet;
+
     // Start is called before the first frame update
     void Start()
     {
         this.minionMovement = new MinionMovement(Enums.MinionPaths.topPathPoints, this.GetComponent<NavMeshAgent>());
+        this.minionAttack = new MinionAttack();
+        this.objectsInRangeHashSet = new HashSet<GameObject>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         this.minionMovement.Move();
+        this.minionAttack.Attack(this.GetTheClosestEnemy());
     }
 
-    private void OnTriggerStay(Collider other)
+    // zamiast tego moze byc lista obiektow, aktualizowana na onenter i onexit,
+    // mozna od razu dodawac tylko te, ktore spelniaja okreslony warunek
+
+    private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.gameObject.name);
+        if (true) // dodac jakie tagi i layery wchodza w sklad tego
+        {
+            this.objectsInRangeHashSet.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        this.objectsInRangeHashSet.Remove(other.gameObject);
+    }
+
+    private GameObject GetTheClosestEnemy()
+    {
+        if (this.objectsInRangeHashSet.Count > 0)
+        {
+            return null;
+        }
+
+        GameObject resGameObject = null;
+
+        foreach (GameObject enemy in this.objectsInRangeHashSet)
+        {
+            if (resGameObject.Equals(null))
+            {
+                resGameObject = enemy;
+            }
+            else if (
+                Utility.GetDistanceBetweenGameObjects(enemy, this.gameObject) < Utility.GetDistanceBetweenGameObjects(resGameObject, this.gameObject)
+                )
+            {
+                resGameObject = enemy;
+            }
+        }
+
+        return resGameObject;
     }
 }
