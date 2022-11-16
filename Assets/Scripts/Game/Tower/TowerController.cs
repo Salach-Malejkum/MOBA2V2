3d65e6b3e2e6cedb37b30e6bc6247e9fb86cc8ac
Topaz-Frog <erwinos753@gmail.com>
2022-11-16 22:38:50 +0100

    added test camera script and fixed turret target

--- Assets/Scripts/Game/Tower/TowerController.cs
+++ Assets/Scripts/Game/Tower/TowerController.cs
@@ -11,13 +11,38 @@ public class TowerController : MonoBehaviour
 
     private List<GameObject> enemies = new List<GameObject>();
     private readonly float missile_delay = 5f;
+    private int targetedLayer;
+
+    private void Awake()
+    {
+        int redLayer = LayerMask.NameToLayer("Red");
+        int blueLayer = LayerMask.NameToLayer("Blue");
+
+        if (this.gameObject.layer == redLayer)
+        {
+            this.targetedLayer = blueLayer;
+        } 
+        else if (this.gameObject.layer == blueLayer)
+        {
+            this.targetedLayer = redLayer;
+        }
+    }
 
     private void FixedUpdate()
     {
         if (this.enemies.Count > 0 && this.missile_timer < 0)
         {
-            Shoot();
-        } else if (this.missile_timer >= 0)
+            foreach (GameObject enemy in this.enemies)
+            {
+                if (enemy == null)
+                {
+                    this.enemies.Remove(enemy);
+                }
+            }
+
+            this.Shoot();
+        }
+        else if (this.missile_timer >= 0)
         {
             this.missile_timer -= Time.deltaTime;
         }
@@ -25,7 +50,7 @@ public class TowerController : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer != this.gameObject.layer && !other.gameObject.CompareTag("Missile"))
+        if (other.gameObject.layer == this.targetedLayer && !other.gameObject.CompareTag("Missile"))
         {
             UnityEngine.Debug.Log(other.gameObject.tag + " in");
             this.enemies.Add(other.gameObject);
@@ -34,7 +59,7 @@ public class TowerController : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.layer != this.gameObject.layer && !other.gameObject.CompareTag("Missile"))
+        if (other.gameObject.layer == this.targetedLayer && !other.gameObject.CompareTag("Missile"))
         {
             UnityEngine.Debug.Log(other.gameObject.tag + " out");
             this.enemies.Remove(other.gameObject);
@@ -45,7 +70,7 @@ public class TowerController : MonoBehaviour
     {
         GameObject tMin = null;
         float minDist = Mathf.Infinity;
-        Vector3 currentPos = transform.position;
+        Vector3 currentPos = this.transform.position;
         foreach (GameObject go in enemies)
         {
             float dist = Vector3.Distance(go.transform.position, currentPos);
@@ -63,7 +88,7 @@ public class TowerController : MonoBehaviour
         GameObject go = Instantiate(this.missile, this.transform);
         HomingMissileController hmc = go.GetComponent<HomingMissileController>();
 
-        hmc.target = GetClosestEnemy(enemies);
+        hmc.target = this.GetClosestEnemy(this.enemies);
 
         this.missile_timer = this.missile_delay;
     }
