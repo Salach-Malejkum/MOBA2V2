262fd81d6c16fec117b133d5b555dcad804c4eab
Mateusz Salach <mateuszsalach00@gmail.com>
2022-11-17 19:45:14 +0100

    Update spawn manager, homing missile script, enums and tags

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,23 +1,44 @@
+using System.Linq;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
     private float timePassed = 0f;
-    private MinionSpawnerScript leftTeamMinions;
-    private MinionSpawnerScript rightTeamMinions;
+    private MinionSpawnerScript blueTeamTopMinions;
+    private MinionSpawnerScript blueTeamBotMinions;
+    private MinionSpawnerScript redTeamTopMinions;
+    private MinionSpawnerScript redTeamBotMinions;
 
     // Start is called before the first frame update
     private void Start()
     {
-        this.leftTeamMinions = new MinionSpawnerScript
+        this.blueTeamTopMinions = new MinionSpawnerScript
             (
             Enums.TeamMinionSpawnerPosition.blueTeamSpawn,
-            Enums.TeamMinionSpawnerTags.blueTeamTag
+            Enums.TeamMinionsLayers.blueTeamLayer,
+            Enums.MinionPaths.topPathPoints
             );
-        this.rightTeamMinions = new MinionSpawnerScript
+
+        this.blueTeamBotMinions = new MinionSpawnerScript
+            (
+            Enums.TeamMinionSpawnerPosition.blueTeamSpawn,
+            Enums.TeamMinionsLayers.blueTeamLayer,
+            Enums.MinionPaths.botPathPoints
+            );
+
+        this.redTeamTopMinions = new MinionSpawnerScript
             (
             Enums.TeamMinionSpawnerPosition.redTeamSpawn,
-            Enums.TeamMinionSpawnerTags.redTeamTag
+            Enums.TeamMinionsLayers.redTeamLayer,
+            Enumerable.Reverse(Enums.MinionPaths.topPathPoints).ToArray()
+            );
+
+
+        this.redTeamBotMinions = new MinionSpawnerScript
+            (
+            Enums.TeamMinionSpawnerPosition.redTeamSpawn,
+            Enums.TeamMinionsLayers.redTeamLayer,
+            Enumerable.Reverse(Enums.MinionPaths.botPathPoints).ToArray()
             );
     }
 
@@ -25,8 +46,10 @@ public class SpawnManager : MonoBehaviour
     private void FixedUpdate()
     {
         this.timePassed += Time.deltaTime;
-        this.SpawnMinions(this.leftTeamMinions);
-        this.SpawnMinions(this.rightTeamMinions);
+        this.SpawnMinions(this.blueTeamTopMinions);
+        this.SpawnMinions(this.blueTeamBotMinions);
+        this.SpawnMinions(this.redTeamTopMinions);
+        this.SpawnMinions(this.redTeamBotMinions);
     }
 
     private void SpawnMinions(MinionSpawnerScript team)
@@ -42,19 +65,22 @@ public class SpawnManager : MonoBehaviour
         for (int i = 0; i < 3; i++)
         {
             GameObject go = Instantiate(team.GetMeleeMinionPrefab(), team.GetSpawnPosition(), Quaternion.identity);
-            go.tag = team.GetTagName();
+            go.layer = team.GetLayer();
+            go.GetComponent<MinionScript>().SetMinionPath(team.GetMinionPath());
         }
 
         if (team.CheckIfCanSpawnCannon())
         {
             GameObject go = Instantiate(team.GetCannonMinionPrefab(), team.GetSpawnPosition(), Quaternion.identity);
-            go.tag = team.GetTagName();
+            go.layer = team.GetLayer();
+            go.GetComponent<MinionScript>().SetMinionPath(team.GetMinionPath());
         }
 
         for (int i = 0; i < 3; i++)
         {
             GameObject go = Instantiate(team.GetRangedMinionPrefab(), team.GetSpawnPosition(), Quaternion.identity);
-            go.tag = team.GetTagName();
+            go.layer = team.GetLayer();
+            go.GetComponent<MinionScript>().SetMinionPath(team.GetMinionPath());
         }
     }
 }
