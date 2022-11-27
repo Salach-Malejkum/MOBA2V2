01d65fc33100127ccbd704e20d27000728808ca9
Erwin Marysiok <63302251+Topaz-Frog@users.noreply.github.com>
2022-11-27 15:50:43 +0100

    Dar 24 biw (#36)
    * Fixed movement
    
    * statystyki bez synchronizacji
    
    * i forgor,
    
    * fix
    
    * Ignore trigger colliders (#18)
    
    * Add files via upload
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Add scripts for player skills and input
    
    * Add prefabs for skills
    
    * Update existing prefabs
    
    * Update existing scripts
    
    * Update Map scene
    
    * Update ProjectSettings and Pacakage jsons
    
    * Show/Hide menu with button
    
    Dodano możliwość pokazywania i ukrywania menu budowania za pomocą przycisku
    
    * cherry-pick dar-185
    
    * Add stats to player prefab, update CrowdControl script to manage stuns
    
    * Update .gitignore
    
    * Move player scripts to Player folder, refactor code
    
    * Create player prefab
    
    * Add player animations
    
    * Update map, spawn points, monsters on map
    
    * Update outline on mobs and towers
    
    * Remove temporary player script and prefab
    
    * Update Lobby scene
    
    * Add attack speed to the unit stats
    
    * Add neutral layer
    
    * Add imported player assets
    
    * merge Mati
    
    * Added budowlanka
    
    * Delete BuildingMenuManager.cs
    
    Co-authored-by: Flixirr <filip.0krezel@gmail.com>
    Co-authored-by: Salach-Malejkum <57152223+Salach-Malejkum@users.noreply.github.com>
    Co-authored-by: Filip Krężel <45013654+Flixirr@users.noreply.github.com>
    Co-authored-by: Mateusz Salach <mateuszsalach00@gmail.com>
    Co-authored-by: K0O7 <57181698+K0O7@users.noreply.github.com>

--- Assets/Units/Scripts/StructureStats.cs
+++ Assets/Units/Scripts/StructureStats.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using System.Linq;
 
 public class StructureStats : UnitStats
 {
+    [SyncVar][SerializeField] protected List<StructureUpgradeSo> activeUpgrades = new();
+    [SyncVar][SerializeField] protected List<StructureUpgradeSo> upgrades = new();
+
     public override void OnStartServer() {
         this.unitCurrentHealth = this.unitMaxHealth;
         this.onUnitDeath += HandleStructureDestroyed;
@@ -15,7 +19,26 @@ public class StructureStats : UnitStats
     }
 
     [Server]
+    public void ActivateUpgrade(int index)
+    {
+        StructureUpgradeSo upgrade = upgrades.ElementAt(index);
+        this.activeUpgrades.Add(upgrade);
+        this.ModifyStats(upgrade);
+    }
+
+    [Server]
     private void HandleStructureDestroyed() {
         NetworkServer.Destroy(this.gameObject);
+    }
+
+    [Server]
+    public virtual void ModifyStats(StructureUpgradeSo structureUpgradeSo)
+    {
+        this.unitMaxHealth += structureUpgradeSo.HealthModifier;
+        this.unitCurrentHealth += structureUpgradeSo.HealthModifier;
+        this.unitArmor += structureUpgradeSo.ArmorModifier;
+        this.unitMagicResist += structureUpgradeSo.MagicResistModifier;
+        this.unitAttackDamage += structureUpgradeSo.AttackDamageModifier;
+        this.unitMovementSpeed += structureUpgradeSo.MovementSpeedModifier;
     }
 }
